00e69a46a4e01522ebba4358582d9c09977863e7
Yunt <PeterLiangAP@Outlook.com>
2020-03-01 05:01:21 -0500

    enable cors

--- music-service/app.js
+++ music-service/app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const logger = require("morgan");
 const createError = require('http-errors');
+const cors = require("cors");
 
-const cfg = require("./config/service.config.js");
+const cfg = require("./config/config.js");
 
 // Prepare Controller
 const indexRouter = require("./routes/index");
@@ -16,6 +17,7 @@ const app = express();
 app.set("port", cfg.port);
 
 // Set up middleware
+app.use(cors())
 app.use(logger("dev")); // This is console logger
 app.use(express.json()); // This is replacement for body-parser
 app.use(express.urlencoded({ extended: false }));
@@ -24,6 +26,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/", indexRouter);
 
 // Register controller
+app.get("/", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.status(200).send({ status: "success" });
+})
+app.get("/music", musicController.list.bind(musicController));
 app.get("/music/album/:albumId/song/:songId", musicController.get.bind(musicController));
 
 // Create a http error for any undefined routes - createError only creates a basic http error object
