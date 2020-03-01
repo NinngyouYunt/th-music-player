a0d5867b526c66ceabe3faa58a412db7a3982bd9
Dehou Kong <124kdaniel@gmail.com>
2020-02-29 21:24:01 -0500

    add endpoint test

--- music-service/routes/index.js
+++ music-service/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/test-music-1', function(req, res, next) {
+  res.sendFile(path.resolve('./assets/test1.mp3'))
 });
 
 module.exports = router;
